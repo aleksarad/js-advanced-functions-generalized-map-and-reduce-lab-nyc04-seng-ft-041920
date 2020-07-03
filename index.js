8511d78a5251b975094b7eef2b9fbcfd7af3e512
aleksarad <alexa.radevich1@gmail.com>
2020-07-03 17:48:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function map(arr, fn) {
+    const newArr = []
+    arr.forEach(function(item){
+        newArr.push(fn(item))
+    })
+    return newArr
+}
+
+function reduce(arr, fn, starting) {
+    let total;
+    let iterationStart;
+    if (starting) {
+        total = starting
+        iterationStart = 0;
+    }
+    else {
+        total = arr[0]
+        iterationStart = 1;
+    }
+    
+    for(let i = iterationStart; i < arr.length; i++) {
+        total = fn(arr[i], total)
+    }
+
+    return total
+}
